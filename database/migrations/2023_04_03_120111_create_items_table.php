17d555c0eb9a1de115ed63acca48e7f041eeb24d
iftekharul-islam <iftekharulislam.cse@gmail.com>
2023-04-04 03:24:47 +0600

    model create

--- database/migrations/2023_04_03_120111_create_items_table.php
+++ database/migrations/2023_04_03_120111_create_items_table.php
@@ -13,6 +13,29 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('order_5p')->nullable()->index('order_5p');
+            $table->string('order_id')->nullable()->index('order_id');
+            $table->string('store_id')->nullable()->index('store_id');
+            $table->unsignedBigInteger('manufacture_id')->nullable()->index('manufacture_id');
+            $table->string('item_code')->nullable()->index('item_code');
+            $table->string('child_sku')->nullable()->index('child_sku');
+            $table->string('item_description')->nullable();
+            $table->string('item_id')->nullable()->index('item_id');
+            $table->string('item_option')->nullable();
+            $table->integer('item_quantity')->nullable();
+            $table->string('item_thumb')->nullable();
+            $table->decimal('item_unit_price', 8,2)->default('0.00');
+            $table->string('item_url')->nullable();
+            $table->string('item_taxable')->nullable();
+            $table->string('tracking_number')->nullable()->index('tracking_number');
+            $table->string('batch_number')->nullable()->index('batch_number');
+            $table->string('data_parse_type')->nullable();
+            $table->string('data_parse_type')->nullable();
+            $table->string('item_status')->index('item_status')->default('1');
+            $table->string('reached_shipping_station')->default('0');
+            $table->string('sure3d')->nullable();
+            $table->string('edi_id')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/migrations/2023_04_03_162518_create_rejections_table.php
+++ database/migrations/2023_04_03_162518_create_rejections_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('rejections', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('item_id')->index('item_id')->nullable();
+            $table->string('complete')->nullable();
+            $table->tinyInteger('graphic_status')->nullable();
+            $table->integer('scrap')->nullable();
+            $table->integer('rejection_reason')->nullable();
+            $table->string('rejection_message')->nullable();
+            $table->integer('reject_qty')->nullable();
+            $table->integer('rejection_user_id')->nullable();
+            $table->string('supervisor_message')->nullable();
+            $table->unsignedBigInteger('supervisor_user_id')->index('supervisor_user_id')->nullable();
+            $table->unsignedBigInteger('from_station_id')->index('from_station_id')->nullable();
+            $table->unsignedBigInteger('to_station_id')->index('to_station_id')->nullable();
+            $table->string('from_batch')->nullable();
+            $table->string('to_batch')->nullable();
+            $table->string('from_screen')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
