864c3171ca7ad97978c53a9baf982ba1274f981a
imrul Afnan <imrul.afnan18@gmail.com>
2023-05-16 18:39:50 +0600

    Complete store

--- app/Http/Controllers/StoreItemController.php
+++ app/Http/Controllers/StoreItemController.php
@@ -2,9 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Store;
+use App\Models\StoreItem;
 use Illuminate\Http\Request;
 
 class StoreItemController extends Controller
 {
-    //
+    public function index(string $id)
+    {
+        $store = Store::with('store_items')->find($id);
+        if (!$store) return response()->json(['message' => 'Store not found', 'status' => 203], 203);
+        return $store;
+    }
+
+    public function store(Request $request)
+    {
+
+        if (!$request->has('store_id')) {
+            return response()->json(['message' => 'Store ID is required', 'status' => 203], 203);
+        }
+        if (!$request->has('vendor_sku') || !$request->has('parent_sku')) {
+            return response()->json(['message' => 'Vendor SKU and Parent SKU are required', 'status' => 203], 203);
+        }
+
+        $item = new StoreItem();
+        $item->store_id = $request->get('store_id');
+        $item->custom = $request->get('custom');
+        $item->vendor_sku = $request->get('vendor_sku');
+        $item->description = $request->get('description');
+        $item->cost = $request->get('cost');
+        $item->parent_sku = $request->get('parent_sku');
+        $item->child_sku = $request->get('child_sku');
+        $item->url = $request->get('url');
+        $item->upc = $request->get('upc');
+        $item->save();
+        return response()->json(['message' => 'Item Added', 'status' => 201], 201);
+    }
+
+    public function update(Request $request)
+    {
+
+        if (!$request->has('store_id')) {
+            return response()->json(['message' => 'Store ID is required', 'status' => 203], 203);
+        }
+        if (!$request->has('vendor_sku') || !$request->has('parent_sku')) {
+            return response()->json(['message' => 'Vendor SKU and Parent SKU are required', 'status' => 203], 203);
+        }
+
+        $item = StoreItem::find($request->get('id'));
+        if (!$item) return response()->json(['message' => 'Item not found', 'status' => 203], 203);
+
+        $item->store_id = $request->get('store_id');
+        $item->custom = $request->get('custom');
+        $item->vendor_sku = $request->get('vendor_sku');
+        $item->description = $request->get('description');
+        $item->cost = $request->get('cost');
+        $item->parent_sku = $request->get('parent_sku');
+        $item->child_sku = $request->get('child_sku');
+        $item->url = $request->get('url');
+        $item->upc = $request->get('upc');
+        $item->save();
+
+        return response()->json(['message' => 'Item Updated', 'status' => 201], 201);
+    }
+
+    public function delete(string $id)
+    {
+        $item = StoreItem::find($id);
+        if (!$item) return response()->json(['message' => 'Item not found', 'status' => 203], 203);
+        $item->is_deleted = '1';
+        $item->save();
+
+        return response()->json(['message' => 'Item Delete', 'status' => 201], 201);
+    }
 }
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -11,6 +11,44 @@ class Store extends Model
 
     public static $companies = ['null' => 'Select Company', '0' => 'Monogramonline', '1' => 'Natico', '2' => 'PWS', '3' => 'Dropship'];
 
+    public static function inputOptions()
+    {
+        return [
+            '0' => 'None',
+            '1' => 'API / FTP (EDI Class)',
+            '2' => 'WebHook',
+            '3' => 'File Import'
+        ];
+    }
+
+    public static function batchOptions()
+    {
+        return [
+            '0' => 'Together with other stores',
+            '1' => 'Separately',
+            '2' => 'Separately at Import'
+        ];
+    }
+
+    public static function notifyOptions()
+    {
+        return [
+            '0' => 'None',
+            '1' => 'API / FTP (EDI Class)',
+            '2' => 'E-mail to Customer',
+            '3' => 'E-mail and API',
+            '4' => 'Export File'
+        ];
+    }
+
+    public static function qcOptions()
+    {
+        return [
+            '0' => 'Normal QC',
+            '1' => 'QC by Shipping Admin'
+        ];
+    }
+
     public static function list($batch = '%', $company = '%', $prepend = ['', ''])
     {
         $array =  Store::where('is_deleted', '0')
