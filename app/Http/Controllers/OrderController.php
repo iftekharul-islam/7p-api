50d42f3a2407453ce6a24be306bb6ea55eba3830
iftekharul islam <iftekharulislam.cse@gmail.com>
2024-11-27 01:08:06 +0600

    product add on name

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2589,9 +2589,10 @@ class OrderController extends Controller
     public function convertImage(Request $request)
     {
         // replace the child sku backslash
+        $product = $request->get('product');
         $child_sku = str_replace('/', '-', $request->child_sku);
         $imagePath = '/media/RDrive/archive/' . $request->get('img_name');
-        $savePath = '/media/RDrive/archive/'. $request->get('batch') .'-'. $child_sku . '-QTY-'. $request->get('QTY') .'.jpg';
+        $savePath = '/media/RDrive/archive/'. $request->get('batch') .'-'. $child_sku . '-'.$product.'-QTY-'. $request->get('QTY') .'.jpg';
 
         if (!file_exists($imagePath)) {
             return [
